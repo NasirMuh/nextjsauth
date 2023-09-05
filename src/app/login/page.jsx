24e5a1d8b79672f807b8edb4f149fd98349a19cb
NasirMuh <octobernine1995@gmail.com>
2023-09-05 11:03:25 +0100

    login with github

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,21 +1,34 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import { signIn, signOut, useSession } from 'next-auth/react'
 
 const Login = () => {
     const session = useSession();
     console.log(session)
+    console.log(session.data?.user?.email)
     if (session.status === "loading") {
         return <p>loading...</p>
     }
     if (session.status === "authenticated") {
-        return <button onClick={() => signOut("google")}>LogOut</button>
+        return (
+            <>Welcome to {session.data?.user?.email}
+                <button onClick={() => signOut()}>LogOut</button>
+
+                {/* <button onClick={() => signOut("google")}>LogOut</button> */}
+            </>
+        )
     }
     if (session.status === "unauthenticated") {
-        return <button onClick={() => signIn("google")}>Sign In With Google</button>
+        return (
+            <>
+                <button onClick={() => signIn("github")}> GitHub | </button>
+
+                <button onClick={() => signIn("google")}> Google</button>
+            </>
+        )
     }
     return (
-        <div>login page</div>
+        <div></div>
     )
 }
 
